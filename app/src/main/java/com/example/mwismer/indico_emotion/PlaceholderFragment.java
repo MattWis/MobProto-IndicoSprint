ed2a6e52900bb0e107d16b4692c750ca40c3d546
MattWis <matthew.wismer@students.olin.edu>
2014-10-02 19:55:41 -0400

    Use intents to display pictures

--- app/src/main/java/com/example/mwismer/indico_emotion/PlaceholderFragment.java
+++ app/src/main/java/com/example/mwismer/indico_emotion/PlaceholderFragment.java
@@ -1,15 +1,33 @@
 package com.example.mwismer.indico_emotion;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PlaceholderFragment extends Fragment {
+    private final static String DEBUG_TAG = "PlaceholderFragment";
+    private static final int REQUEST_CODE = 1;
+    private Bitmap bitmap;
+    private ImageView imageView;
 
     public PlaceholderFragment() {
     }
@@ -18,6 +36,64 @@ public class PlaceholderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        imageView = (ImageView) rootView.findViewById(R.id.result);
+
+        final Button cam = (Button) rootView.findViewById(R.id.camera);
+        cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
         return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("HI", "Activity :" + imageView);
+        InputStream stream = null;
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            try {
+                // recycle unused bitmaps
+                if (bitmap != null) {
+                    bitmap.recycle();
+                }
+                stream = getActivity().getContentResolver().openInputStream(data.getData());
+                bitmap = BitmapFactory.decodeStream(stream);
+
+                // 4096 x 4096 is the biggest image we can display
+                // It errors if you try to crop an image to larger than it's size
+                if (bitmap.getWidth() > 4096 || bitmap.getHeight() > 4096) {
+                    int width = 4096;
+                    int height = 4096;
+                    if (bitmap.getWidth() < 4096) {
+                        width = bitmap.getWidth();
+                    }
+                    if (bitmap.getHeight() < 4096) {
+                        height = bitmap.getHeight();
+                    }
+
+                    imageView.setImageBitmap(Bitmap.createBitmap(bitmap, 0, 0, width, height));
+                } else {
+                    imageView.setImageBitmap(bitmap);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if (stream != null) {
+                    try {
+                        stream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
